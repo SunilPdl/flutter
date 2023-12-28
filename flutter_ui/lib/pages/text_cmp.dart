fd24693a8013234848b25c00c64ab1ea4ff420a8
SunilPdl <mrsunilpaudel@gmail.com>
2023-12-28 17:18:14 +0545

    button , text and button style

--- flutter_ui/lib/pages/text_cmp.dart
+++ flutter_ui/lib/pages/text_cmp.dart
@@ -5,51 +5,135 @@ class TextComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    int result = 0;
+    final TextEditingController textEditingController = TextEditingController();
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(50)),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Text Component'),
+        centerTitle: true,
+      ),
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "This is my first text ",
-            style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: (Color.fromRGBO(255, 0, 0, 1))),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              // label:
-              // Text(
-              //   "Enter your name...",
-              //   style: TextStyle(color: Colors.black),
-              // ),
-              // helperText: "Helper text...",
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "This is my first text ",
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: (Color.fromRGBO(255, 0, 0, 1))),
+            ),
+            Container(
+              //if only padding property is required then we used Padding
+              // padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              margin: const EdgeInsets.only(bottom: 20.0),
+              child: TextField(
+                controller: textEditingController,
+                decoration: InputDecoration(
+                  // label:
+                  // Text(
+                  //   "Enter your name...",
+                  //   style: TextStyle(color: Colors.black),
+                  // ),
+                  // helperText: "Helper text...",
 
-              hintText: "Enter your name",
-              hintStyle: TextStyle(
-                // color: Color.fromARGB(150, 69, 69, 69),
-                color: Color.fromRGBO(69, 69, 69, 0.9),
-                // color: Colors.black54,
-              ),
-              prefixIcon: Icon(Icons.people_alt_outlined),
-              prefixIconColor: Color.fromRGBO(69, 69, 69, 0.9),
+                  hintText: "Enter your name",
+                  hintStyle: const TextStyle(
+                    // color: Color.fromARGB(150, 69, 69, 69),
+                    color: Color.fromRGBO(69, 69, 69, 0.9),
+                    // color: Colors.black54,
+                  ),
+                  prefixIcon: const Icon(Icons.people_alt_outlined),
+                  prefixIconColor: const Color.fromRGBO(69, 69, 69, 0.9),
 
-              // suffixIcon: Icon(Icons.people_alt_outlined),
-              // suffixIconColor: Color.fromRGBO(69, 69, 69, 0.9),
+                  // suffixIcon: Icon(Icons.people_alt_outlined),
+                  // suffixIconColor: Color.fromRGBO(69, 69, 69, 0.9),
 
-              filled: true,
-              fillColor: Colors.black26,
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  width: 2.0,
-                  style: BorderStyle.solid,
+                  filled: true,
+                  fillColor: Colors.black26,
+                  // focusedBorder: OutlineInputBorder(
+                  //   borderSide: BorderSide(
+                  //     width: 2.0,
+                  //     style: BorderStyle.solid,
+                  //   ),
+                  //   borderRadius: BorderRadius.all(Radius.circular(50)),
+                  // borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+                  // borderRadius: BorderRadius.circular(60);
+                  //The constructor being called isn't a const constructor.Try removing 'const' from the constructor -> remove parent const and adding const only to child
+                  focusedBorder: border,
+                  enabledBorder: border,
                 ),
+                keyboardType: TextInputType.number,
+                // keyboardType: TextInputType.numberWithOptions(
+                //   decimal: true,
+                //   signed: true,
+                // ),
               ),
             ),
-          )
-        ],
-      )),
+            //Normal/Flat button
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            //   child: TextButton(
+            //     onPressed: () {
+            //       debugPrint("Button clicked");
+            //     },
+            //     style: const ButtonStyle(
+            //       backgroundColor: MaterialStatePropertyAll(Colors.black),
+            //       foregroundColor: MaterialStatePropertyAll(Colors.white),
+            //       minimumSize: MaterialStatePropertyAll(
+            //         Size(double.infinity, 60),
+            //       ),
+            //     ),
+            //     child: const Text("Click me"),
+            //   ),
+            // ),
+
+            // Eleveted/Raised button
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            //   child: ElevatedButton(
+            //     onPressed: () {
+            //       debugPrint("Button clicked");
+            //     },
+            //     style: const ButtonStyle(
+            //       backgroundColor: MaterialStatePropertyAll(Colors.black),
+            //       foregroundColor: MaterialStatePropertyAll(Colors.white),
+            //       minimumSize: MaterialStatePropertyAll(
+            //         Size(double.infinity, 60),
+            //       ),
+            //       // elevation: MaterialStatePropertyAll(6),
+            //       // shape: MaterialStatePropertyAll(CircleBorder()),
+            //     ),
+            //     child: const Text("Click me"),
+            //   ),
+            // ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  print(textEditingController.text);
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 60),
+                ),
+                child: const Text("Click me"),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
