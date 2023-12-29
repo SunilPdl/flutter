68f59d526103a8a4ba1526b6c2cc7896f935dd4e
SunilPdl <mrsunilpaudel@gmail.com>
2023-12-29 09:08:08 +0545

    statefull widget

---
+++ flutter_ui/lib/pages/money_converter.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class MoneyConverter extends StatefulWidget {
+  const MoneyConverter({super.key});
+
+  @override
+  State<MoneyConverter> createState() => _MoneyConverterState();
+}
+
+class _MoneyConverterState extends State<MoneyConverter> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
