9ebb0d26da499d273c73238c2a4c2ba6d8f74e1e
SunilPdl <mrsunilpaudel@gmail.com>
2024-01-08 12:05:37 +0545

    add : router

--- flutter_ui/lib/main.dart
+++ flutter_ui/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui/pages/ui/listview-gridview/grid_view.dart';
+import 'package:flutter_ui/router/app_route_config.dart';
 // import 'package:flutter_ui/pages/money_converter.dart';
 // import 'package:flutter_ui/pages/ui/listview-gridview/listView.dart';
 // import 'package:flutter_ui/pages/text_cmp.dart';
@@ -12,13 +13,20 @@ class MyApp extends StatelessWidget {
   // const MyApp({Key? key}) : super(key: key);
   const MyApp({super.key});
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   return const MaterialApp(
+  //     debugShowCheckedModeBanner: false,
+  //     // home: MoneyConverter(),
+  //     // home: ListViewExample(),
+  //     home: GridViewExample(),
+  //   );
+  // }
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      // home: MoneyConverter(),
-      // home: ListViewExample(),
-      home: GridViewExample(),
+      routerConfig: MyRoute().router,
     );
   }
 }
